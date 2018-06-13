1f4e800a6c5c68afc36357ae43e8e162b2f265e4
Lea Hughes <lahughesmusic@gmail.com>
2018-06-13 17:49:52 -0500

    add files

--- liri.js
+++ liri.js
@@ -11,26 +11,27 @@ var omdb = require('omdb');
 var spotify = new Spotify(keys.spotify);
  
 
- 
-// var getArtistNames = function(artist) {
-//   return artist.name;
-// };
-
+var command = process.argv[2]
 var thing = process.argv[3];
 var thing2 = process.argv[4];
 
 
 
 
+
+// This was a challenger for me, I never got my answers to show up organized. 
+
+
+
+
 // OMDB
+var omdbRun = function(){
 var request = require("request");
 var queryUrl = "http://www.omdbapi.com/?t=" + thing + "&y=&plot=short&apikey=trilogy";
 
 console.log(queryUrl);
 
 request(queryUrl, function(error, response, body) {
-
-  // If the request is successful
   if (!error && response.statusCode === 200) {
 
 
@@ -38,9 +39,15 @@ request(queryUrl, function(error, response, body) {
     console.log("Release Year: " + JSON.parse(body).Year);
   }
 });
+}
+
+
+
+
+
 
 // SPOTIFY
-var spot= function() {
+var spotified= function() {
 
   spotify.search(
     {
@@ -56,8 +63,6 @@ var spot= function() {
     }
   );
 };
-
-spot();
 
 
 
@@ -79,12 +84,22 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
 });
 }
 
-tweetTweet();
 
+
+
+
+// I don't think I'm supposed to have this section, but I couldn't figure out how else to make it only run the api it was supposed to run 
  
- 
 
-
+if(command === 'movie-this'){
+  omdbRun();
+}
+if(command === 'my-tweets'){
+  tweetTweet();
+}
+if (command === 'spotify-this-song'){
+  spotified();
+}
 
 
 
